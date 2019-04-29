af985233dd2afe1f8276c795e323f5daf1ba6502
Christopher Patton <chpatton013@gmail.com>
2019-04-29 10:07:25 -0700

    Add install-tmux role

---
+++ roles/install-tmux/templates/tmux.sh
@@ -0,0 +1,7 @@
+export TMUX_VERSION="{{ tmux_version }}"
+export TMUX_SOURCE_ROOT="{{ tmux_source_root }}"
+export TMUX_BUILD_ROOT="{{ tmux_build_root }}"
+export TMUX_INSTALL_ROOT="{{ tmux_install_root }}"
+
+export TPM_VERSION="{{ tpm_version }}"
+export TPM_ROOT="{{ tpm_root }}"
