a54d1f70180bd137d24f7af6a9132c7d5d4c3833
Christopher Patton <chpatton013@gmail.com>
2019-04-29 11:05:00 -0700

    Update python and ruby variables

--- roles/install-python/templates/python.sh
+++ roles/install-python/templates/python.sh
@@ -1,4 +1,15 @@
-export PYENV_ROOT="{{ pyenv_root }}"
 export PYENV_VERSION="{{ pyenv_version }}"
+export PYENV_ROOT="{{ pyenv_root }}"
+export PYENV_VERSIONS_ROOT="{{ pyenv_versions_root }}"
+
 export PYTHON2_VERSION="{{ python2_version }}"
+export PYTHON2_ROOT_DIRECTORY="{{ python2_root_directory }}"
+export PYTHON2_BIN_DIRECTORY="{{ python2_bin_directory }}"
+export PYTHON2_LIB_DIRECTORY="{{ python2_lib_directory }}"
+export PYTHON2_CONFIG_DIRECTORY="{{ python2_config_directory }}"
+
 export PYTHON3_VERSION="{{ python3_version }}"
+export PYTHON3_ROOT_DIRECTORY="{{ python3_root_directory }}"
+export PYTHON3_BIN_DIRECTORY="{{ python3_bin_directory }}"
+export PYTHON3_LIB_DIRECTORY="{{ python3_lib_directory }}"
+export PYTHON3_CONFIG_DIRECTORY="{{ python3_config_directory }}"
--- roles/install-ruby/templates/ruby.sh
+++ roles/install-ruby/templates/ruby.sh
@@ -1,4 +1,11 @@
-export RBENV_ROOT="{{ rbenv_root }}"
 export RBENV_VERSION="{{ rbenv_version }}"
+export RBENV_ROOT="{{ rbenv_root }}"
+export RBENV_VERSIONS_ROOT="{{ rbenv_versions_root }}"
+export RBENV_PLUGINS_ROOT="{{ rbenv_plugins_root }}"
+
 export RUBY_BUILD_VERSION="{{ ruby_build_version }}"
+
 export RUBY_VERSION="{{ ruby_version }}"
+export RUBY_ROOT_DIRECTORY="{{ ruby_root_directory }}"
+export RUBY_BIN_DIRECTORY="{{ ruby_bin_directory }}"
+export RUBY_LIB_DIRECTORY="{{ ruby_lib_directory }}"
