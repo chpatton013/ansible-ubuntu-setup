d94312050c7a11d8c466ce0814d05496a6d6a3b0
Christopher Patton <chpatton013@gmail.com>
2019-04-29 11:06:25 -0700

    Fix vim directory structure

--- roles/install-vim/templates/vim.sh
+++ roles/install-vim/templates/vim.sh
@@ -1,3 +1,4 @@
+export VIM_VERSION="{{ vim_version }}"
 export VIM_SOURCE_ROOT="{{ vim_source_root }}"
 export VIM_BUILD_ROOT="{{ vim_build_root }}"
-export VIM_VERSION="{{ vim_version }}"
+export VIM_INSTALL_ROOT="{{ vim_install_root }}"
