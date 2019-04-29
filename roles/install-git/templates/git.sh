57d16649e3554f31dcbcb24d535e76ef7830f4aa
Christopher Patton <chpatton013@gmail.com>
2019-04-29 12:21:49 -0700

    Fix git directory structure

--- roles/install-git/templates/git.sh
+++ roles/install-git/templates/git.sh
@@ -1,3 +1,4 @@
+export GIT_VERSION="{{ git_version }}"
 export GIT_SOURCE_ROOT="{{ git_source_root }}"
 export GIT_BUILD_ROOT="{{ git_build_root }}"
-export GIT_VERSION="{{ git_version }}"
+export GIT_INSTALL_ROOT="{{ git_install_root }}"
