4b6940d3407ecca3fd1a1a8c5c5666aeb72d4cdc
Christopher Patton <chpatton013@gmail.com>
2019-05-06 11:59:51 -0700

    REBASE INTO GIT

--- roles/install-git/templates/git.sh
+++ roles/install-git/templates/git.sh
@@ -1,4 +1,3 @@
 export GIT_VERSION="{{ git_version }}"
 export GIT_SOURCE_ROOT="{{ git_source_root }}"
-export GIT_BUILD_ROOT="{{ git_build_root }}"
 export GIT_INSTALL_ROOT="{{ git_install_root }}"
