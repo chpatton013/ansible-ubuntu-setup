656d8016eebbd2d9af68068eeec25de3f39a5887
Christopher Patton <chpatton013@gmail.com>
2019-05-07 16:11:36 -0700

    REBASE INTO VIM

--- roles/install-vim/templates/vim.sh
+++ roles/install-vim/templates/vim.sh
@@ -1,4 +1,3 @@
 export VIM_VERSION="{{ vim_version }}"
 export VIM_SOURCE_ROOT="{{ vim_source_root }}"
-export VIM_BUILD_ROOT="{{ vim_build_root }}"
 export VIM_INSTALL_ROOT="{{ vim_install_root }}"
